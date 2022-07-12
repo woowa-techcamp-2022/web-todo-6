bf3ffc226eff36508f76114f1c188e15af6c72f7
kokocan12 <sky203614@gmail.com>
2022-07-12 21:01:41 +0900

    todo section ui개발 완료

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,5 +1,6 @@
 import { TodoAside,TodoAsideCloseIcon, TodoLog } from './TodoAside.js' 
 import { TodoHeader, TodoHeaderTitle, TodoHeaderMenu } from './TodoHeader.js' 
+import { TodoSection, TodoSectionHeader } from './TodoSection.js'
 
 customElements.define('todo-header',TodoHeader)
 customElements.define('todo-header-title',TodoHeaderTitle)
@@ -8,3 +9,6 @@ customElements.define('todo-header-menu',TodoHeaderMenu)
 customElements.define('todo-aside-close-icon',TodoAsideCloseIcon)
 customElements.define('todo-aside',TodoAside)
 customElements.define('todo-log', TodoLog)
+
+customElements.define('todo-section', TodoSection)
+customElements.define('todo-section-header', TodoSectionHeader)
