3acd485fe2b0b2fe264835242034c088d9da32ce
jstella96 <jstella0530@naver.com>
2022-07-14 08:58:10 +0900

    todo card move api 구현

--- server/api/todo.js
+++ server/api/todo.js
@@ -8,19 +8,22 @@ router.post('/', async (req, res) => {
     const { title, contents, todoSectionId} = req.body
     insertTodo( title, contents, todoSectionId).then( todo => {
         //const logData = { action: "추가" ,todoCardId : todo.insertId, todoSectionId: todoSectionId}
-            setTodoLog("추가",todo.insertId, todoSectionId).then(todo => {
+        setTodoLog("추가",todo.insertId, todoSectionId).then(todo => {
             res.status(200).json(todo)
         })
     })
 })
 
-
+//여기서 sectionID 검색해도 되긴합니다. 
 router.patch('/:id', (req, res) => {
-    const { title, contents} = req.body;
+    const { title, contents, todoSectionId } = req.body;
     const updateData = { title, contents}
     const { id } = req.params;
-    if( !id || (  !title && !contents )) return;
+    if( !id || ( !title && !contents || !todoSectionId )) return res.status(500);
     updateTodo( id, updateData ).then( todo => {
+        setTodoLog("수정",id, todoSectionId).then(todo => {
+            res.status(200).json(todo)
+        })
         res.status(200).json(todo)
     })
     
@@ -29,9 +32,11 @@ router.patch('/:id/move', (req, res) => {
     const { todoSectionId } = req.body;
     const updateData = { todoSectionId }
     const { id } = req.params;
-    if( !id || ! todoSectionId ) return;
+    if( !id || ! todoSectionId ) return res.status(500);
     updateTodo( id, updateData ).then( todo => {
-        res.status(200).json(todo)
+        setTodoLog("이동", id, todoSectionId).then(todo => {
+            res.status(200).json(todo)
+        })
     })
     
 })
@@ -42,6 +47,5 @@ router.delete('/:id', (req, res) => {
         res.status(200).json(todo)
     })
 })
-
 
 module.exports = router;
