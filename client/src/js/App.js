6acd56d5ddd515c9e5e7a5d2bf5a9b057bacb781
이형준 <49009864+kokocan12@users.noreply.github.com>
2022-07-13 19:44:03 +0900

    [Feature] 섹션 간 카드 이동 파란선 표시 되도록 개발 (#26)
    * insert blue line 개발 완료
    
    * blue line margin 수정
    
    * main no-drag 처리

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,6 +1,6 @@
 import { TodoAside,TodoAsideCloseIcon, TodoLog } from './TodoAside.js' 
 import { TodoHeader, TodoHeaderTitle, TodoHeaderMenu } from './TodoHeader.js' 
-import { TodoSection, TodoSectionHeader } from './TodoSection.js'
+import { TodoSection, TodoSectionHeader, BlueLine } from './TodoSection.js'
 import { TodoCard } from './TodoCard.js'
 import { TodoMain } from './TodoMain.js'
 
@@ -18,3 +18,4 @@ customElements.define('todo-log', TodoLog)
 customElements.define('todo-section', TodoSection)
 customElements.define('todo-section-header', TodoSectionHeader)
 customElements.define('todo-card', TodoCard)
+customElements.define('blue-line', BlueLine)
--- client/src/js/TodoSection.js
+++ client/src/js/TodoSection.js
@@ -2,8 +2,111 @@ class TodoSection extends HTMLElement {
     constructor() {
         super()
     }
+
+    get width() {
+        return this.clientWidth;
+    }
+
+    get left() {
+        return this.offsetLeft;
+    }
     
     connectedCallback(){
+        this.addEventListener('pointerenter', this.handlePointerEnter)
+        this.addEventListener('pointermove', this.handlePointerMove)
+        this.addEventListener('pointerleave', this.handlePointerLeave)
+    }
+
+    handlePointerEnter() {
+        const $blueline = document.createElement('blue-line')
+        $blueline.setAttribute('width', this.width);
+        $blueline.setAttribute('left', this.left);
+        
+        this.appendChild($blueline);
+        this.$blueline = $blueline;
+        
+    }
+
+    handlePointerMove(evt) {
+        const THROTTLE_TIME = 100;
+        const currentTime = new Date().valueOf();
+        
+        if(this.lastMoveEventTime && (currentTime - this.lastMoveEventTime < THROTTLE_TIME)) {
+            return
+        }
+
+        this.lastMoveEventTime = currentTime;
+        
+        const cards = [];
+        for(let i=0; i<this.children.length; i++) {
+            const child = this.children[i];
+            if(child.tagName === 'TODO-CARD') {
+                cards.push(child);
+            }
+        }
+
+        const mouseY = evt.clientY;
+        const windowScrollY = window.scrollY
+        const BLUE_LINE_MARGIN = 5;
+        // this.children[0] is always TODO-SECTION-HEADER.
+        // Default bluelineY is TODO-SECTION-HEADER's bottom.
+        const $todoSectionHeader = this.children[0];
+        let bluelineY = $todoSectionHeader.offsetTop + $todoSectionHeader.clientHeight - windowScrollY;
+        
+        for(let i=0; i<cards.length; i++) {
+            const $card = cards[i];
+            
+            const leftTop = $card.offsetTop - windowScrollY;
+            const rightBottom = $card.offsetTop + $card.clientHeight - windowScrollY;
+
+            if((bluelineY<mouseY) && ((leftTop < mouseY && mouseY < rightBottom) || (i === cards.length - 1))) {
+                bluelineY = rightBottom + BLUE_LINE_MARGIN;
+                break;
+            }
+        }
+
+        this.$blueline.setAttribute('top', bluelineY)
+
+    }
+
+    handlePointerLeave() {
+        this.removeChild(this.$blueline)
+    }
+}
+
+class BlueLine extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    static get observedAttributes() { return ['width', 'left', 'top']; }
+
+    connectedCallback() {
+        const width = this.getAttribute('width')
+        const left = this.getAttribute('left')
+        const top = this.getAttribute('top')
+
+        this.style.display = 'block'
+        this.style.position = 'fixed'
+        this.style.backgroundColor = 'rgba(9, 105, 218, 0.3)'
+        this.style.height = '3px';
+        this.style.width = `${width}px`
+        this.style.left = `${left}px`
+        this.style.top = `${top}px`
+    }
+
+    attributeChangedCallback(name, _, newValue) {
+        switch(name) {
+            case 'width':
+                this.style.width = `${newValue}px`
+                break 
+            case 'left':
+                this.style.left = `${newValue}px`
+                break 
+            case 'top':
+                this.style.top = `${newValue}px`
+                break
+        }
     }
 }
 
@@ -36,4 +139,4 @@ class TodoSectionHeader extends HTMLElement {
 }
 
 
-export { TodoSection, TodoSectionHeader }
+export { TodoSection, TodoSectionHeader, BlueLine }
