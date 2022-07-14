5c2a02ddc5b1d79c84737f49a8ec510fcebe3dbc
jstella96 <jstella0530@naver.com>
2022-07-14 15:57:27 +0900

    todo log api 구현 완료

--- server/api/todoLog.js
+++ server/api/todoLog.js
@@ -1,11 +1,17 @@
 const express = require('express')
 
 const router = express.Router();
-const { getTodoLogs } = require('../repo/todoLogRepo')
+const { getTodoLogs ,setTodoLog } = require('../repo/todoLogRepo')
 
 
 router.get('/', (req, res) => {
     getTodoLogs().then(logs => {
+        res.status(200).json(logs);
+    })
+});
+router.post('/', (req, res) => {
+    const { action, todoTitle, todoSection }  = req.body
+    setTodoLog(action, todoTitle, todoSection).then(logs => {
         res.status(200).json(logs);
     })
 });
