686f3dca5286072fb3944be515a59d4a757f5674
jstella96 <jstella0530@naver.com>
2022-07-15 08:10:50 +0900

    생성, 수정, 삭제 log 미들웨어 구현

--- server/middleware/log.js
+++ server/middleware/log.js
@@ -1,12 +1,27 @@
 const { setTodoLog } = require('../repo/todoLogRepo')
+const { getTodoById } = require('../repo/todoRepo')
 
-function setLog(logData){
-    const {action, todoCardId ,todoSectionId} = logData
-    setTodoLog(action, todoCardId ,todoSectionId).then(logs => {
-        console.log(logs)
-        return;
+function setLog(req,res){
+
+    const { action, id } = req.logData
+    if(action === '삭제'){
+        const { todoTitle, sectionTitle } = req.deleteLogData
+            setTodoLog(action, todoTitle, sectionTitle).then( () => {
+                return res.status(200).json(req.data) 
+        })
+    }else{
+    
+    getTodoById(id).then(todo => {
+        isArray = Array.isArray(todo)
+        if(isArray){
+            todo = todo[0]
+        }
+        const { todoTitle, sectionTitle } = todo
+        setTodoLog(action, todoTitle, sectionTitle).then( () => {
+            return res.status(200).json(req.data) 
+        })
     })
-
+    }
 }
 module.exports = {
     setLog
